82993326d07b0a356798b679a4d59f531883ac1b
Dimitrije <sovljanski04@gmail.com>
2024-06-15 18:30:23 +0200

    chore: Remove unused code in ContactPage.tsx and update form attributes for Netlify integration

--- src/views/pages/ContactPage.tsx
+++ src/views/pages/ContactPage.tsx
@@ -7,6 +7,7 @@ export default function ContactPage() {
         name="contact"
         method="POST"
         data-netlify="true"
+        data-netlify-honeypot="bot-field"
         onSubmit={() => alert("Thank you for your message!")}
         className="flex bg-black bg-opacity-10 flex-col gap-8 h-full w-[50%] grid place-self-center p-8 rounded-xl shadow-lg text-center"
         initial={{ opacity: 0, y: -100 }}
